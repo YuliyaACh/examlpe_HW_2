b6274d70223b7907d5c96dad4e79dbe9fc6dd6ff
Your Name <you@example.com>
2024-06-27 23:22:03 +0500

    hw 1 done

--- SearchTests.java
+++ SearchTests.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
